bea60f19d1eb2089a959c63a745cf4cad1a905f4
leweex95 <leweex95@users.noreply.github.com>
2025-10-22 22:58:31 +0200

    added plotly-based visualization module with interactive charts

--- src/vlm_chart_pattern_analyzer/__init__.py
+++ src/vlm_chart_pattern_analyzer/__init__.py
@@ -4,10 +4,28 @@ __version__ = "0.1.0"
 
 from .models import load_model, get_available_models, MODEL_REGISTRY
 from .inference import run_inference
+from .visualization import (
+    load_benchmark_results,
+    plot_latency_by_model_precision,
+    plot_memory_by_model_precision,
+    plot_latency_vs_memory,
+    plot_tokens_generated,
+    plot_model_comparison_heatmap,
+    plot_comprehensive_dashboard,
+    create_summary_statistics,
+)
 
 __all__ = [
     "load_model",
     "get_available_models",
     "MODEL_REGISTRY",
     "run_inference",
+    "load_benchmark_results",
+    "plot_latency_by_model_precision",
+    "plot_memory_by_model_precision",
+    "plot_latency_vs_memory",
+    "plot_tokens_generated",
+    "plot_model_comparison_heatmap",
+    "plot_comprehensive_dashboard",
+    "create_summary_statistics",
 ]
